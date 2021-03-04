94890555708aa879234e83ec9a2c54ae8542f382
Clayon Stewart <c.stewart89@gmail.com>
2021-03-04 01:47:06 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,7 +37,7 @@ end
 
 
 def full?(board)
-  empty = board.select { |space| (space == "" || space == " ")  }
+  empty = board.each { |space| (space == "" || space == " ")  }
   if empty
     return true
   else
@@ -55,7 +55,6 @@ end
 
 def win_combination?(board, indexes)
   if (board[indexes[0]] == "X" && board[indexes[1]] == "X" && board[indexes[2]] == "X") || (board[indexes[0]] == "O" && board[indexes[1]] == "O" && board[indexes[2]] == "O") 
-    r
     return true
   end
 end
