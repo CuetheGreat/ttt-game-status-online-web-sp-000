7d879f7f3a1aa9bdff87812be791fdca832e0d51
Clayon Stewart <c.stewart89@gmail.com>
2021-03-04 01:17:40 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,7 +23,7 @@ def won?(board)
     return nil
   else
     WIN_COMBINATIONS.each do |win_combo| 
-      if (board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X") 
+      if (board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X") || (board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O")
         return win_combo
       end
     end
